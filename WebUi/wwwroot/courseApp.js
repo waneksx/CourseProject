da72eac9e5464c93038ed7c8fc71523658f73713
wanek96 <wanek96@mail.ru>
2017-02-26 12:29:16 +0200

    cmt

---
+++ WebUi/wwwroot/courseApp.js
@@ -0,0 +1,33 @@
+﻿var courseApp = angular.module("courseApp", []);
+courseApp.controller("mapController", function (mapService) {
+    this.initialize = function () {
+        mapService.load()
+    }
+});
+
+courseApp.service('mapService', function () {
+    return {
+        pointsSource: new ol.source.Vector({
+            features: []
+        }),
+        load: function () {
+            var map = new ol.Map({
+                target: 'map',
+                layers: [
+                  new ol.layer.Tile({
+                      source: new ol.source.OSM()
+                  }),
+                  new ol.layer.Vector({
+                      source: this.pointsSource
+                  })
+                ],
+                view: new ol.View({
+                    center: ol.proj.fromLonLat([37.41, 8.82]),
+                    zoom: 4
+                })
+            });
+            
+        },
+    }
+
+})
